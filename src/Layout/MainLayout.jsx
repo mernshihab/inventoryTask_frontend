e45671f21ebf986f31f6333fd3000ef2b607f4e4
mernshihab <shihabmd103@gmail.com>
2025-06-29 16:11:11 +0600

    responsive sidebar

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,14 +1,27 @@
 import { Link, Outlet } from "react-router-dom";
+import { useState } from "react";
 import "./MainLayout.css";
+import { FaBars } from "react-icons/fa";
 
 export default function MainLayout() {
+  const [showSidebar, setShowSidebar] = useState(false);
+
+  const toggleSidebar = () => {
+    setShowSidebar(!showSidebar);
+  };
+
   return (
     <div className="layout-container">
-      <aside className="sidebar">
+      <div className="mobile-header">
+        <FaBars onClick={toggleSidebar} className="hamburger-icon" />
+        <h2 className="mobile-title">Inventory App</h2>
+      </div>
+
+      <aside className={`sidebar ${showSidebar ? "show" : ""}`}>
         <h2 className="sidebar-title">Dashboard</h2>
         <nav className="nav-links">
-          <Link to="/">Products</Link>
-          <Link to="/categories">Categories</Link>
+          <Link to="/" onClick={() => setShowSidebar(false)}>Products</Link>
+          <Link to="/categories" onClick={() => setShowSidebar(false)}>Categories</Link>
         </nav>
       </aside>
 
